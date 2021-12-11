32039617aa98fb86e4b50d8e8669b75094d5376e
TeemuVolanen <teemu.k.volanen@student.jyu.fi>
2021-12-11 20:46:18 +0200

    moka

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
@@ -21,7 +21,9 @@ const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
-  
-export default { 
+
+const personService = { 
   getAll, create, deletePerson, update
 }  
+  
+export default personService
